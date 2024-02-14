185a86cbc5052d4d1f2e39d2c1bffc78775cc14a
hidekiabe <hideki-abe@hotmail.com>
2024-02-14 11:01:22 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  //templateUrl: './app.component.html',
+  template: '<main>\n' +
+    '    <header class="brand-name">\n' +
+    '      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">\n' +
+    '    </header>\n' +
+    '    <section class="content">\n' +
+    '      <app-home></app-home>\n' +
+    '    </section>\n' +
+    '  </main>\n' +
+    '`',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
